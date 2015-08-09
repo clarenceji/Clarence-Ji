5becbc536b81f0d3f07cc47f09ad474fbc0f4a61
Clarence Ji <clare.dev@clarenceji.net>
2015-08-09 20:32:37 +0800

    WatchKit Extension + Notification Center Extension

---
+++ Clarence in Notification Center/TodayViewController.swift
@@ -0,0 +1,40 @@
+//
+//  TodayViewController.swift
+//  Clarence in Notification Center
+//
+//  Created by Clarence Ji on 5/28/15.
+//  Copyright (c) 2015 Clarence Ji. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view from its nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func widgetMarginInsetsForProposedMarginInsets(defaultMarginInsets: UIEdgeInsets) -> UIEdgeInsets {
+        return UIEdgeInsetsZero
+    }
+    
+    
+    
+    func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)!) {
+        // Perform any setup necessary in order to update the view.
+
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+
+        completionHandler(NCUpdateResult.NoData)
+    }
+    
+}
